e24f954ad97d4f2bbb2e6255fe201fe62e5b8883
Weten-Ogen <alphaoware@gmail.com>
2023-02-14 13:11:59 +0000

    fixings bugs on the views

--- backend/cfehome/product/urls.py
+++ backend/cfehome/product/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('<int:pk>/', views.ProductDetailApiView.as_view()),
-   path('<int:pk>/delete/',views.ProductDeleteApiView.as_view()),
-   path('<int:pk>/update/',views.ProductUpdateApiView.as_view()),
    path('', views.ProductCreateApiView.as_view()),
    path('list/', views.ProductListApiView.as_view()),
+   path('<int:pk>/',views.ProductDetailApiView.as_view()),
+   path('<int:pk>/delete/',views.ProductDeleteApiView.as_view()),
+   path('<int:pk>/update/',views.ProductUpdateApiView.as_view()),
 ]
